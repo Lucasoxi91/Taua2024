da50bd52fc6b7f87674e48a72fea137e1f451e38
Lucasoxi91 <57160026+Lucasoxi91@users.noreply.github.com>
2024-05-28 10:45:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_login import LoginManager, UserMixin, login_user, login_required
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_bcrypt import Bcrypt
 import os
 import importlib.util
@@ -18,18 +18,16 @@ def get_db_connection():
         database="de84slt1iucctv",
         user="adaptativa_read",
         password="pe71d6441e182e2458c5fd7701d60d1d0023f68f74dbd0ea0f8e1211d05a14374",
-        port="5432",
-        connect_timeout=30
+        port="5432"
     )
 
 class User(UserMixin):
-    def __init__(self, id, username):  # Corrigido para usar dois sublinhados de cada lado
+    def __init__(self, id, username):
         self.id = id
         self.username = username
 
     def get_id(self):
         return str(self.id)
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -61,88 +59,28 @@ def login():
                 return render_template('login.html', error="Usuário ou senha inválidos")
     return render_template('login.html')
 
-
-def fetch_pie_chart_data():
-    conn = get_db_connection()
-    try:
-        with conn.cursor() as cur:
-            cur.execute("""
-                        WITH Alunos8Ano AS (
-                    SELECT 
-                        i.id AS institution_id,
-                        i.name AS institution_name,
-                        COUNT(DISTINCT ie.user_id) AS alunos_8ano
-                    FROM 
-                        institution_enrollments ie
-                    INNER JOIN institution_classrooms ic ON ic.id = ie.classroom_id  
-                    INNER JOIN institution_levels il ON il.id = ic.level_id 
-                    INNER JOIN institution_courses ic3 ON ic3.id = il.course_id 
-                    INNER JOIN institution_colleges ic2 ON ic2.id = ic3.institution_college_id 
-                    INNER JOIN institutions i ON i.id = ic2.institution_id
-                    INNER JOIN regions r ON ic2.region_id = r.id 
-                    INNER JOIN cities c ON c.id = r.city_id 
-                    WHERE i.name ILIKE '%2024%'
-                    AND c.name = 'Tauá'
-                    AND i.id IN (335, 337)
-                    AND ic.name ILIKE '%8%'
-                    GROUP BY i.id, i.name
-                ),
-                TodosAlunosMatriculados AS (
-                    SELECT 
-                        i.id AS institution_id,
-                        i.name AS institution_name,
-                        COUNT(DISTINCT ie.user_id) AS alunos_matriculados
-                    FROM 
-                        institution_enrollments ie
-                    INNER JOIN institution_classrooms ic ON ic.id = ie.classroom_id  
-                    INNER JOIN institution_levels il ON il.id = ic.level_id 
-                    INNER JOIN institution_courses ic3 ON ic3.id = il.course_id 
-                    INNER JOIN institution_colleges ic2 ON ic2.id = ic3.institution_college_id 
-                    INNER JOIN institutions i ON i.id = ic2.institution_id
-                    INNER JOIN regions r ON ic2.region_id = r.id 
-                    INNER JOIN cities c ON c.id = r.city_id 
-                    WHERE i.name ILIKE '%2024%'
-                    AND c.name = 'Tauá'
-                    AND i.id IN (335, 336, 337, 338)
-                    AND ic.name NOT ILIKE '%8%'
-                    GROUP BY i.id, i.name
-                )
-                SELECT 
-                    T.institution_id,
-                    T.institution_name,
-                    COALESCE(T.alunos_matriculados, 0) AS total_matriculados
-                FROM 
-                    TodosAlunosMatriculados T
-                LEFT JOIN Alunos8Ano A 
-                    ON T.institution_id = A.institution_id
-                ORDER BY T.institution_id;
-            """)
-            results = cur.fetchall()
-            if results:
-                labels = [row[1] for row in results]
-                data = [row[2] for row in results]  # Total de alunos matriculados
-                return {'labels': labels, 'data': data}
-    except Exception as e:
-        print(f"Erro ao executar consulta SQL: {e}")
-    finally:
-        conn.close()
-    return {'labels': [], 'data': []}
-
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("https://taua2024-5674f4a1f8a4.herokuapp.com/login?next=%2F")
 
 def list_scripts():
+    """Lista os scripts Python disponíveis na pasta 'scripts'."""
     scripts = [f for f in os.listdir(SCRIPTS_DIR) if f.endswith('.py')]
     print("Scripts disponíveis:", scripts)
     return scripts
 
 def execute_script(script_name, filters=None):
+    """Executa a função 'execute_query' do script selecionado e retorna seus resultados."""
     script_path = os.path.join(SCRIPTS_DIR, script_name)
     print("Executando script:", script_path)
     spec = importlib.util.spec_from_file_location("module.name", script_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     
+    # Adiciona filtros se forem fornecidos
     if filters:
-        print("Executando com filtros:", filters)
         columns, result = module.execute_query(filters)
     else:
         columns, result = module.execute_query()
@@ -153,20 +91,18 @@ def execute_script(script_name, filters=None):
 @login_required
 def index():
     scripts = list_scripts()
-    pie_chart_data = fetch_pie_chart_data()
-    return render_template('index.html', scripts=scripts, pie_chart_data=pie_chart_data)
+    return render_template('index.html', scripts=scripts)
 
-@app.route('/tabela', methods=['GET'])
+@app.route('/execute', methods=['GET'])
 @login_required
-def tabela():
-    script_name = request.args.get('script_name')
-    print("Nome do script solicitado:", script_name)
+def execute():
+    script_name = request.args.get('script')
+    display_mode = request.args.get('display', 'tabela')
+
     if script_name in list_scripts():
-        filters = {key: value for key, value in request.args.items() if key != 'script_name'}
-        columns, data = execute_script(script_name, filters)
-        return render_template('tabela.html', columns=columns, data=data, script_name=script_name)
+        columns, data = execute_script(script_name)
+        return render_template('tabela.html', columns=columns, data=data)
     else:
-        print("Script não encontrado:", script_name)
         return "Script not found", 404
 
 @app.route('/filter_data', methods=['GET'])
@@ -175,12 +111,15 @@ def filter_data():
     script_name = request.args.get('script_name')
     print("Nome do script solicitado para filtro:", script_name)
     if script_name in list_scripts():
-        filters = {key: value for key, value in request.args.items() if key != 'script_name'}
+        filters = {}
+        for key, value in request.args.items():
+            if key not in ('script_name',):
+                filters[key] = value
+
         print("Filtros aplicados:", filters)
         columns, data = execute_script(script_name, filters)
         return jsonify(columns=columns, data=data)
     else:
-        print("Script não encontrado para filtro:", script_name)
         return jsonify(columns=[], data=[])
 
 if __name__ == '__main__':
