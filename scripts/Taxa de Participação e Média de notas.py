dc94dbb53bf256e6c437f67978456718c048ae13
Lucasoxi91 <57160026+Lucasoxi91@users.noreply.github.com>
2024-05-29 15:26:58 -0300

    Update Taxa de Participação e Média de notas.py

--- scripts/Taxa de Participação e Média de notas.py
+++ scripts/Taxa de Participação e Média de notas.py
@@ -72,7 +72,7 @@ SELECT
     qd.simulado,
     qd.n_user_id as finalizaram,
     ed.matriculados,
-    ROUND((CAST(qd.n_user_id AS numeric) / GREATEST(ed.matriculados, 1)) * 100, 1) AS participação,  -- Arredondado para uma casa decimal
+    ROUND((CAST(qd.n_user_id AS numeric) / GREATEST(ed.matriculados, 1)) * 100, 1) AS Taxa_de_Participação,  -- Arredondado para uma casa decimal
     qd.grade_avg AS média_notas
 FROM QuizData qd
 INNER JOIN EnrollmentData ed ON qd.curso = ed.curso AND qd.escola_id = ed.escola_id
